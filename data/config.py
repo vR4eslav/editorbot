1ca5989f353107d0f0ec58bbdf8eac736c42f249
valisov <kldgrcorp@gmail.com>
2021-09-18 19:19:23 +0300

    added email init

--- data/config.py
+++ data/config.py
@@ -9,3 +9,5 @@ ADMINS = env.list("ADMINS")  # Тут у нас будет список из а�
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
 ADVEGO_TOKEN = env.str('ADVEGO_TOKEN')
+
+email = env.str('email')
