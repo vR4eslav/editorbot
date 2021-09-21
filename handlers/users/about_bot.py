80ed34a07368cf758edad533fa008a4215fa6a52
valisov <kldgrcorp@gmail.com>
2021-09-21 22:13:02 +0300

    added info about bot

--- handlers/users/about_bot.py
+++ handlers/users/about_bot.py
@@ -8,8 +8,10 @@ from loader import dp
 @dp.callback_query_handler(text='about_bot')
 async def cq_about(call: CallbackQuery):
     keyboard = types.InlineKeyboardMarkup()
-    btn5 = types.InlineKeyboardButton(text='🔙 НАЗАД В МЕНЮ 🔙', callback_data='back_to_menu')
+    btn5 = types.InlineKeyboardButton(text='Код GitHub', url='https://github.com/vR4eslav/editorbot')
     keyboard.add(btn5)
+    btn6 = types.InlineKeyboardButton(text='🔙 НАЗАД В МЕНЮ 🔙', callback_data='back_to_menu')
+    keyboard.add(btn6)
     await call.message.edit_text(text=f"Отправь текст, который хочешь проверить, и получи результат! Объем одной "
                                       f"проверки — 4096 символов.\n"
                                       # f"Хочешь проверить документ на уникальность? Просто отправь файл!\n"
@@ -18,5 +20,6 @@ async def cq_about(call: CallbackQuery):
                                       f"Посчитать символы в тексте\n"
                                       f"Поменять регистр сообщения\n"
                                       f"Проверить текст на орфографию\n\n"
-                                      f"⚠️Возникли трудности или предложения — пиши: {config.email}",
+                                      f"Сгенерировать надежный пароль\n\n"
+                                      f"⚠️Возникли трудности или предложения — пиши: {config.email}\n\n",
                                  reply_markup=keyboard)
