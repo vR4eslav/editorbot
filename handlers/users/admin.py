7ec05ba31d73e1cac75ed0890b9d3c6c7ec72eb2
valisov <kldgrcorp@gmail.com>
2021-10-03 20:58:01 +0300

    initialization admin panel(soon)

---
+++ handlers/users/admin.py
@@ -0,0 +1,7 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+from loguru import logger
+
+from handlers.users.broadcast_handler import broadcaster
+from loader import dp
+
