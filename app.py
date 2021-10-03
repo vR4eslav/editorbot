9cdb0c23fb0801c316fa7fa901f719ddf1b7d17a
valisov <kldgrcorp@gmail.com>
2021-10-03 21:02:13 +0300

    db connection release

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from aiogram import executor
+from loguru import logger
 
-from loader import dp
+from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -12,7 +13,7 @@ async def on_startup(dispatcher):
 
     # Уведомляет про запуск
     await on_startup_notify(dispatcher)
-
-
+    await db.create()
+    await db.create_table_users()
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
